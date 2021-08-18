5f06320adc14367bf3c50fafba45b10b945fa9a5
alexandre gomes <alecxandy@gmail.com>
2021-08-18 00:23:47 -0300

    create class domain

---
+++ src/main/java/domain/PlayList.java
@@ -0,0 +1,58 @@
+package domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "playlist")
+public class PlayList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 60)
+    @Column(nullable = false, length = 60)
+    private String descricao;
+    private String nome;
+
+    @NotBlank
+    @Column(nullable = false)
+    public Long getId() {
+        return id;
+    }
+
+    @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL)
+    private List<Musica> musicas;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+}
